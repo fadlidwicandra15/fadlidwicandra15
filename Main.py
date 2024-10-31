a01e4adfb4e1c2fc3a9205cd93939e32f3a511c4
fadlidwicandra15 <fadlidwicandra@gmail.com>
2024-10-31 06:01:23 -0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+# input user
+
+# data input dari user
+data = input("masukan data : ")
+
+print("data : ", data, "type = ", type(data))
+
+# jika data adalah angka lakukan casting
+angka = float (input ("masukkan data : " ))
+
+print("data : ", angka, "type = ", type(angka))
+
+# bagaimana jika data adalah boolean
